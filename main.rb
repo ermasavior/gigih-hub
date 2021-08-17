6159aa839a5fe4ed579f05214f2c4245f31f6c39
Erma Safira N <erma.safira@gmail.com>
2021-08-17 15:14:03 +0700

    Add POST /api/users endpoint

--- main.rb
+++ main.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+require_relative 'controllers/user_controller'
 
 get '/' do
   return "Hello World from Gigih Hub API"
+end
+
+post '/api/users' do
+  controller = UserController.new
+  controller.create(params)
 end
