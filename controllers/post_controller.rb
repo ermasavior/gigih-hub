9cd8708d87c3fdb7775fd53707b84e31e5d88b19
Erma Safira N <erma.safira@gmail.com>
2021-08-17 17:03:51 +0700

    [GREEN] test PostController.create passed: context to check params

--- controllers/post_controller.rb
+++ controllers/post_controller.rb
@@ -4,6 +4,9 @@ class PostController
   def create(params)
     user = User.find_by_id(params['user_id'])
     post = Post.new(user: user, text: params['text'])
-    post.save
+    save_success = post.save
+
+    return { status: 200 } if save_success
+    { status: 400 }
   end
 end
