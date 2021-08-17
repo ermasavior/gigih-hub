798bf8d68612de3a8716cd942fc9918d362f3ada
Erma Safira N <erma.safira@gmail.com>
2021-08-17 13:52:53 +0700

    [GREEN] test User.new passed to save username, email, and bio

--- models/user.rb
+++ models/user.rb
@@ -4,5 +4,9 @@ class User
   attr_accessor :id, :username, :email, :bio
 
   def initialize(id=nil, username:, email:, bio:)
+    @id = id,
+    @username = username,
+    @email = email,
+    @bio = bio
   end
 end
