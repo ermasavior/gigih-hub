99584ff5ef00a87bc48c8aae69bdbdba32f9a22c
Erma Safira N <erma.safira@gmail.com>
2021-08-17 17:03:02 +0700

    [RED] add test PostController.create: context to check params

--- spec/controllers/post_controller_spec.rb
+++ spec/controllers/post_controller_spec.rb
@@ -18,5 +18,39 @@ RSpec.describe 'PostController' do
       controller = PostController.new
       controller.create(params)
     end
+
+    context 'check params' do
+      before do
+        allow(User).to receive(:find_by_id).with(user.id).and_return(user)
+        allow(Post).to receive(:new).with(text: text, user: user)
+          .and_return(post_stub)
+      end
+
+      context 'when params are valid' do
+        let(:status) { 200 }
+
+        it 'returns status 200' do
+          allow(post_stub).to receive(:save).and_return(true)        
+
+          controller = PostController.new
+          response = controller.create(params)
+
+          expect(response).to eq(expected_response)
+        end
+      end
+
+      context 'when params are invalid' do
+        let(:status) { 400 }
+
+        it 'returns status 400' do
+          allow(post_stub).to receive(:save).and_return(false)        
+
+          controller = PostController.new
+          response = controller.create(params)
+
+          expect(response).to eq(expected_response)
+        end
+      end
+    end
   end
 end
