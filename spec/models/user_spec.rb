0139e26dacf47cc48225e326cad4b37cf353926b
Erma Safira N <erma.safira@gmail.com>
2021-08-17 14:47:24 +0700

    [RED] add context to test user.save: when params are invalid, it does not trigger query

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,15 +18,32 @@ RSpec.describe 'User' do
   end
 
   describe '.save' do
-    let(:expected_query) { "INSERT INTO users(username, email, bio) VALUES ('#{username}','#{email}','#{bio}')" }
+    context 'when params are valid' do
+      let(:expected_query) { "INSERT INTO users(username, email, bio) VALUES ('#{username}','#{email}','#{bio}')" }
 
-    it 'triggers query to insert new user' do
-      expect(User.client).to receive(:query).with(expected_query).once
+      it 'triggers query to insert new user' do
+        expect(User.client).to receive(:query).with(expected_query).once
 
-      user = User.new(
-        username: username, email: email, bio: bio
-      )
-      user.save
+        user = User.new(
+          username: username, email: email, bio: bio
+        )
+        user.save
+      end
+    end
+
+    context 'when params are invalid' do
+      let(:username) { nil }
+      let(:email) { nil }
+      let(:bio) { nil }
+
+      it 'does not trigger query' do
+        expect(User.client).not_to receive(:query)
+
+        user = User.new(
+          username: username, email: email, bio: bio
+        )
+        user.save
+      end
     end
   end
 end
