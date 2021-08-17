c507c0b934b6071ad795825e3e9b7b437800aafc
Erma Safira N <erma.safira@gmail.com>
2021-08-17 16:16:35 +0700

    [GREEN] test Post.new passed: initialize

--- models/post.rb
+++ models/post.rb
@@ -4,5 +4,9 @@ class Post < Model
   attr_accessor :id, :text, :created_at, :user
 
   def initialize(id=nil, created_at=nil, text:, user:)
+    @id = id
+    @created_at = created_at
+    @text = text
+    @user = user
   end
 end
