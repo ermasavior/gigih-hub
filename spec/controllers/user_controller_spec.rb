c14579db6cbcac3f984c1b644da51cb3da9990a1
Erma Safira N <erma.safira@gmail.com>
2021-08-17 15:34:16 +0700

    [RED] Add test for response status in user controller

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe 'UserController' do
 
   describe '.create' do
     let(:user_stub) { double }
+    let(:expected_response) { { status: status } }
 
     it 'calls user.save with params' do
       expect(User).to receive(:new).with(username: username, email: email, bio: bio)
@@ -16,6 +17,39 @@ RSpec.describe 'UserController' do
 
       controller = UserController.new
       controller.create(params)
+    end
+
+    context 'check params' do
+      before do
+        allow(User).to receive(:new).with(username: username, email: email, bio: bio)
+          .and_return(user_stub)
+      end
+
+      context 'when params are valid' do
+        let(:status) { 200 }
+
+        it 'returns status 200' do
+          allow(user_stub).to receive(:save).and_return(true)        
+
+          controller = UserController.new
+          response = controller.create(params)
+
+          expect(response).to eq(expected_response)
+        end
+      end
+
+      context 'when params are invalid' do
+        let(:status) { 400 }
+
+        it 'returns status 400' do
+          allow(user_stub).to receive(:save).and_return(false)        
+
+          controller = UserController.new
+          response = controller.create(params)
+
+          expect(response).to eq(expected_response)
+        end
+      end
     end
   end
 end
