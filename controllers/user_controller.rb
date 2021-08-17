50c326761ce470d1e07d86b59bea460f53496641
Erma Safira N <erma.safira@gmail.com>
2021-08-17 15:02:56 +0700

    [GREEN] test UserController.create calls user.save passed

--- controllers/user_controller.rb
+++ controllers/user_controller.rb
@@ -2,6 +2,9 @@ require_relative '../models/user'
 
 class UserController
   def create(params)
-    
+    user = User.new(
+      username: params['username'], email: params['email'], bio: params['bio']
+    )
+    user.save
   end
 end
