b48e4e7b26e9a826e518b0b2e32b057cc58913e7
Erma Safira N <erma.safira@gmail.com>
2021-08-20 00:46:53 +0700

    [GREEN] test PostController create passed: hashtag and post hashtag calls

--- controllers/post_controller.rb
+++ controllers/post_controller.rb
@@ -6,6 +6,11 @@ class PostController
     post = Post.new(user: user, text: params['text'])
     save_success = post.save
 
+    post.hashtags.each do |hashtag|
+      hashtag.save
+      PostHashtag.new(post: post, hashtag: hashtag).save
+    end
+
     return { status: 200 } if save_success
     { status: 400 }
   end
