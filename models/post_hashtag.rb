6c36816ec3af84ae388ba5947f37a10b7dc0ebc4
Erma Safira N <erma.safira@gmail.com>
2021-08-19 23:34:44 +0700

    [RED] add test PostHashtag.new: initialize

---
+++ models/post_hashtag.rb
@@ -0,0 +1,6 @@
+require_relative '../models/model'
+
+class PostHashtag < Model
+  def initialize
+  end
+end
---
+++ spec/models/post_hashtag_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../../models/post_hashtag'
+
+RSpec.describe 'PostHashtag' do
+  let(:user) { User.new(1, username: "cici", email: "abc@gmail.com", bio: nil) }
+  let(:text) { "Assalamualaikum #cantik" }
+  let(:post) { Post.new(1, user: user, text: text)}
+  let(:hashtags) { post.hashtags }
+
+  describe 'initialize' do
+    it 'creates a post hashtag' do
+      post_hashtag = PostHashtag.new(post: post, hashtag: hashtags[0])
+
+      expect(post_hashtag.post).to eq(post)
+      expect(post_hashtag.hashtag).to eq(hashtag)
+    end
+  end
+end
