c234662492d3a9e7096664474dbd637a9c03da6c
Erma Safira N <erma.safira@gmail.com>
2021-08-19 23:59:05 +0700

    [GREEN] test post_hashtag.save passed: does not triggers insert query when params are invalid

--- models/post_hashtag.rb
+++ models/post_hashtag.rb
@@ -9,6 +9,8 @@ class PostHashtag < Model
   end
 
   def save
+    return false if @post.nil? or @hashtag.nil?
+
     PostHashtag.client.query("INSERT INTO post_hashtags(post_id, hashtag_id) VALUES ('#{@post.id}','#{@hashtag.id}')")
     true
   end
