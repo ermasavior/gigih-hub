6cb1f4567f578d3bf57d30a513f475d2a2a62b45
Erma Safira N <erma.safira@gmail.com>
2021-08-21 01:01:10 +0700

    [REFACTOR] Add create_comment in separated function

--- controllers/post_controller.rb
+++ controllers/post_controller.rb
@@ -17,4 +17,24 @@ class PostController
 
     { status: 200 }
   end
+
+  def create_comment(params)
+    parent_post_id = params['parent_post_id']
+    return { status: 400 } if parent_post_id.nil? || parent_post_id == ''
+
+    user = User.find_by_id(params['user_id'])
+    post = Post.new(
+      nil, parent_post_id, user: user, text: params['text']
+    )
+    save_success = post.save
+
+    return { status: 400 } unless save_success
+
+    post.hashtags.each do |hashtag|
+      hashtag.save
+      PostHashtag.new(post: post, hashtag: hashtag).save
+    end
+
+    { status: 200 }
+  end
 end
--- spec/controllers/post_controller_spec.rb
+++ spec/controllers/post_controller_spec.rb
@@ -3,13 +3,14 @@ require_relative '../../controllers/post_controller'
 RSpec.describe 'PostController' do
   let(:user) { User.new(1, username: 'erma', email: 'erma@test.com', bio: 'Simple.') }
   let(:text) { 'Lorem ipsum dolor sit amet.' }
-  let(:params) { { 'user_id' => user.id, 'text' => text } }
 
-  describe '.create' do
-    let(:post) { double }
-    let(:hashtags) { [double] }
-    let(:post_hashtag) { double }
-    let(:expected_response) { { status: status } }
+  let(:post) { double }
+  let(:hashtags) { [double] }
+  let(:post_hashtag) { double }
+  let(:expected_response) { { status: status } }
+
+  describe '.create_post' do
+    let(:params) { { 'user_id' => user.id, 'text' => text } }
 
     it 'calls user, post, hashtag, and post_hashtag with params' do
       expect(User).to receive(:find_by_id).with(user.id).and_return(user)
@@ -65,6 +66,70 @@ RSpec.describe 'PostController' do
 
           controller = PostController.new
           response = controller.create_post(params)
+
+          expect(response).to eq(expected_response)
+        end
+      end
+    end
+  end
+
+  describe '.create_comment' do
+    let(:parent_post_id) { 1 }
+    let(:params) { { 'user_id' => user.id, 'parent_post_id' => parent_post_id, 'text' => text } }
+
+    it 'calls user, post, hashtag, and post_hashtag with params' do
+      expect(User).to receive(:find_by_id).with(user.id).and_return(user)
+      expect(Post).to receive(:new).with(nil, parent_post_id, text: text, user: user)
+                                   .and_return(post)
+      expect(post).to receive(:save).and_return(true)
+
+      expect(post).to receive(:hashtags).and_return(hashtags)
+      hashtags.each do |hashtag|
+        expect(hashtag).to receive(:save)
+        expect(PostHashtag).to receive(:new).with(post: post, hashtag: hashtag)
+                                            .and_return(post_hashtag)
+        expect(post_hashtag).to receive(:save)
+      end
+
+      controller = PostController.new
+      controller.create_comment(params)
+    end
+
+    context 'check params' do
+      before do
+        allow(User).to receive(:find_by_id).with(user.id).and_return(user)
+        allow(Post).to receive(:new).with(nil, parent_post_id, text: text, user: user)
+                                    .and_return(post)
+        allow(post).to receive(:hashtags).and_return(hashtags)
+
+        hashtags.each do |hashtag|
+          allow(hashtag).to receive(:save)
+          allow(PostHashtag).to receive(:new).with(post: post, hashtag: hashtag)
+                                             .and_return(post_hashtag)
+          allow(post_hashtag).to receive(:save)
+        end
+      end
+
+      context 'when params are valid' do
+        let(:status) { 200 }
+
+        it 'returns status 200' do
+          allow(post).to receive(:save).and_return(true)
+
+          controller = PostController.new
+          response = controller.create_comment(params)
+
+          expect(response).to eq(expected_response)
+        end
+      end
+
+      context 'when params are invalid' do
+        let(:parent_post_id) { [nil, ''].sample }
+        let(:status) { 400 }
+
+        it 'returns status 400' do
+          controller = PostController.new
+          response = controller.create_comment(params)
 
           expect(response).to eq(expected_response)
         end
