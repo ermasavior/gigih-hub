4bc6d5dbd0d9e20fbc0df8de3fe3607e2cf9124a
Erma Safira N <erma.safira@gmail.com>
2021-08-21 16:16:47 +0700

    [REFACTOR] tidy up AttachmentUploader to pass rubocop lint

--- services/attachment_uploader.rb
+++ services/attachment_uploader.rb
@@ -1,13 +1,15 @@
 class AttachmentUploader
   attr_reader :filename, :tempfile, :base_url
 
-  BASE_FILE_SUBPATH = 'storage/'
-  BASE_FILE_PATH = './public/' + BASE_FILE_SUBPATH
+  BASE_FILE_SUBPATH = 'storage/'.freeze
+  BASE_FILE_PATH = "./public/#{BASE_FILE_SUBPATH}".freeze
 
   def initialize(params)
-    @filename = params['filename'] unless params.nil?
-    @tempfile = params['tempfile'] unless params.nil?
-    @base_url = params['base_url'] unless params.nil?
+    return if params.nil?
+
+    @filename = params['filename']
+    @tempfile = params['tempfile']
+    @base_url = params['base_url']
   end
 
   def upload
